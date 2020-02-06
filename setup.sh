bb2ef808e5709e0c56d73adc77dc75ade5ae1cdd
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-02-05 16:14:21 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+print "Setting up Files and Folders"
 cd /opt/retropie/configs
 sudo mkdir video
 sudo wget https://github.com/CrashCortez/Gpivideo/blob/master/video/emulators.cfg
@@ -9,4 +10,7 @@ cd
 cd /etc/emulationstation
 sudo cp es_systems.cfg es_systems.cfg.old
 sudo weget https://github.com/CrashCortez/Gpivideo/blob/master/es_systems.cfg
+cd
+print "Installing MPV"
+sudo apt-get install mpv
 sudo reboot
