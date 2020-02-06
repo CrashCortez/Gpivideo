fe91829a4d88eff8ef2879c3a81de6befa2a1357
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-02-05 16:29:16 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,21 @@
 print "Setting up Files and Folders"
 cd /opt/retropie/configs
 sudo mkdir video
-sudo wget https://github.com/CrashCortez/Gpivideo/blob/master/video/emulators.cfg
+sudo wget https://raw.githubusercontent.com/CrashCortez/Gpivideo/master/video/emulators.cfg
 cd
 cd /pi/RetroPie/roms
 mkdir videos
 cd
 cd /etc/emulationstation
 sudo mv es_systems.cfg es_systems.cfg.old
-sudo wget https://github.com/CrashCortez/Gpivideo/blob/master/es_systems.cfg
+sudo wget https://raw.githubusercontent.com/CrashCortez/Gpivideo/master/es_systems.cfg
 cd
 print "Installing MPV"
 sudo apt-get install mpv
+clear
+print "GPI will now reboot, dont for get to put some .avi .mp4 .mov files in your videos roms folder."
+wait 5
+clear
+print "See you at the Movies - Crash"
+wait 5
 sudo reboot
