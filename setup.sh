88ea49017cf4681cf505e91970281c6217d6206f
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-02-06 17:10:15 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-print "Setting up Files and Folders"
+echo Setting up Files and Folders
 wait 5 
 clear
 cd /opt/retropie/configs
@@ -13,18 +13,19 @@ sudo chmod -R 777 videos
 cd
 sudo chown -R pi:pi /home/pi/RetroPie/roms
 cd /etc/emulationstation
-print "Renaming your es_systems.cfg to es_systems.cfg.old"
+echo Renaming your es_systems.cfg to es_systems.cfg.old
 wait 5
 clear
 sudo mv es_systems.cfg es_systems.cfg.old
 sudo wget https://raw.githubusercontent.com/CrashCortez/Gpivideo/master/es_systems.cfg
 cd
-print "Installing MPV"
+echo Installing MPV
 sudo apt-get install mpv
-clear
-print "GPI will now reboot, dont for get to put some .avi .mp4 .mov files in your videos roms folder."
 wait 5
 clear
-print "See you at the Movies - Crash"
+echo GPI will now reboot, dont for get to put some .avi .mp4 .mov files in your videos roms folder.
+wait 5
+clear
+echo See you at the Movies - Crash
 wait 5
 sudo reboot
